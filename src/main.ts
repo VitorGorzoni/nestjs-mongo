ae713203a9c59fbf6d74104918d0ad01066af5e7
Vitor Gorzoni <mse_vgorzoni@uolinc.com>
2019-03-19 11:11:21 -0300

    Port initialization nest

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Server } from 'src/config/server';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3500);
+  await app.listen(Server.port);
 }
 bootstrap();
