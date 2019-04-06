efe7bf065fca7054731e26a7f5fd99476417c2d0
Vitor Gorzoni <mse_vgorzoni@uolinc.com>
2019-04-06 00:37:52 -0300

    Fix Deploy Heroku

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AccountModule } from './account/account.module';
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
